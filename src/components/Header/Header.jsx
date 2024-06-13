f4a6175aac829a5eef0c5d7420f6c5bacc0d83e8
SaikatKarar <kararsaikat@gmail.com>
2024-06-13 18:19:36 +0530

    Header Using CSS

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav>
+        <nav style={{ display: 'flex', gap: '16px', justifyContent: 'center' }}>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/review">Order Review</NavLink>
             <NavLink to="/about">About</NavLink>
